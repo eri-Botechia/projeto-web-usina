6a269c359bd58307272716980ae913765571acba
eri-botechia <botechia94.erika@gmail.com>
2024-10-01 03:14:10 -0300

    adicionando modificacoes parciais de troca de paginas

--- maketado/js/app.js
+++ maketado/js/app.js
@@ -1,11 +1,32 @@
-import blog from './templates/blog.js';
-const btnBlog = document.getElementById('btnBlog');
+import blog from './template-informativos/blog.js';
+import home from './template-gerais/home.js';
+import projeto from './template-informativos/projeto.js';
+import usinaSkills from './template-acoes/usinaSkills.js';
 
-const changeContent = (idBtn, element4Setup) => {
-    let mainHTML = document.getElementById('main__content');
-    idBtn.addEventListener('click', () => {
-        mainHTML.innerHTML = element4Setup; // Atualiza o conteúdo ao clicar
+const btnBlog = document.getElementById('btnBlog');
+const btnHome = document.getElementById('btnHome');
+const btnSkills = document.getElementById('btnSkills')
+
+// Function to load initial content
+const initialContent = () => {
+    const mainHTML = document.getElementById('main__content');
+    mainHTML.innerHTML += home; // Load initial content
+};
+
+// Function to change content based on button click
+const changeContent = (button, content) => {
+    button.addEventListener('click', () => {
+        const mainHTML = document.getElementById('main__content');
+        mainHTML.innerHTML = content; // Update content on click
     });
 };
 
-changeContent(btnBlog, blog);
+// Set up event listeners when the DOM is fully loaded
+document.addEventListener('DOMContentLoaded', () => {
+    initialContent(); // Load initial content
+    changeContent(btnBlog, blog); // Set up Blog button
+    changeContent(btnHome, home); 
+    changeContent(btnSkills, usinaSkills);
+    const btnProjetoH= document.getElementById('btnProjetoHero')
+    changeContent(btnProjetoH, projeto)
+});
